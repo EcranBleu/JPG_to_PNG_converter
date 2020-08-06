f823f632975ece2f4c94da581d0efb63b5ab585e
EcranBleu <nclochez@gmail.com>
2020-08-06 11:56:09 +0200

    added program file

---
+++ JPEGtoPNGconverter.py
@@ -0,0 +1,35 @@
+from PIL import Image
+import sys
+import os
+from pathlib import Path
+import shutil
+
+existing_folder = Path(sys.argv[1])
+new_folder = Path(sys.argv[2])
+
+
+# Defining the jpg (and only jpg) image copy into the destination folder.
+
+def jpg_copy():
+    for source_file in existing_folder.iterdir():
+        if str(source_file).lower().endswith(('.jpg', '.jpeg')):
+            shutil.copy(source_file, new_folder)
+
+
+# Create destination folder and apply copy, or just apply copy if the folder already exists.
+
+try:
+    os.mkdir(sys.argv[2])
+    jpg_copy()
+except FileExistsError:
+    jpg_copy()
+
+# Convert the files in the destination folder.
+
+for target_image in new_folder.iterdir():
+    if str(target_image).lower().endswith('.png'):
+        continue
+    img = Image.open(target_image)
+    img_copy = img.copy()
+    img_copy.save(f'{str(target_image).rsplit(".")[0]}.png')
+    os.remove(target_image)
